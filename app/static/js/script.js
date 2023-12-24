8598ffb99993e696daac24a53504c8a3c2a9126b
Serdar Ablyametov <96997312+Serdar17@users.noreply.github.com>
2023-12-24 13:56:32 +0500

    feat: added snack bar

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,17 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const tagLinks = document.querySelectorAll('.nav-link');
-//
-//     console.log(tagLinks.length);
-//     tagLinks.forEach(function(link) {
-//         link.addEventListener('click', function(event) {
-//
-//
-//             tagLinks.forEach(function(otherLink) {
-//                 otherLink.classList.remove('active');
-//             });
-//
-//             link.classList.add('active');
-//             event.preventDefault();
-//         });
-//     });
-// });
+function toastFunction() {
+  var x = document.getElementById("toast");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
